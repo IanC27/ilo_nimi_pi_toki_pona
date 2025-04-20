0b3af0456a21668d3aa22e2e2f5f4fcd82cd3ea2
Ian <ian.k.cowan@gmail.com>
2025-04-19 19:22:54 -0700

    enable contact menu item to translate and open popup

--- background.js
+++ background.js
@@ -4,10 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ autoplay: false });
     chrome.storage.sync.set({ sandbox: "requested" });
     
-    // can't open popups from context menu yet:
-    // https://github.com/GoogleChrome/developer.chrome.com/issues/2602
-    /* 
-    chrome.contextMenus.create({
+    browser.contextMenus.create({
         title: "Translate Toki Pona word \"%s\"",
         id: "translate",
         contexts: ["selection"],
@@ -15,28 +12,13 @@ chrome.runtime.onInstalled.addListener(() => {
         () => {console.log("created menu item");
     });
     
-    chrome.contextMenus.onClicked.addListener((info) => {
+    browser.contextMenus.onClicked.addListener((info) => {
         console.log("ok");
         if (info.menuItemId == "translate") {
             console.log(info.selectionText);
-            chrome.action.openPopup();
+            browser.action.openPopup();
         }
     });
-    */
+    
    
 });
-
-
-// getting the json:
-// https://stackoverflow.com/a/59916857
-// no longer needed with new api.linku.la
-/*
-const getJSON = async url => {
-    const response = await fetch(url);
-    if (!response.ok) // check if response worked (no 404 errors etc...)
-        throw new Error(response.statusText);
-
-    const data = response.json(); // get JSON from the response
-    return data; // returns a promise, which resolves to this data value
-}
-*/
